a4d1d79a310938ca72ab863eef7f2fb3659b712b
Volodimir Vojtik <volodimirvojtik17@gmail.com>
2025-03-17 12:35:43 +0200

    Updated favicon and improved UI styles

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
+import Image from "next/image";
 import TextEditor from "../components/TextEditor";
-import "../styles/TextEditor.css"; // Подключаем стили
+import "../styles/TextEditor.css"; 
 
 export default function Home() {
     return (
         <div className="text-editor-container">
+            <Image src="/logo.png" alt="Logo" width={80} height={80} />
             <h1 className="welcome-text">Grammar Correction App</h1>
             <p className="welcome-text">Welcome! Start typing your text below:</p>
             <TextEditor />
